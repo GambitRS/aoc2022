f031065e5e5510d7bc379608ecd4102d66c92334
GambitRS <hidde.baggelaar@gmail.com>
2023-08-17 23:19:27 +0200

    rewrote into map reduce

--- 2/dist/app.js
+++ 2/dist/app.js
@@ -1,18 +1,9 @@
 import { promises as fs } from 'fs';
 import * as url from 'url';
-const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 const text = await fs.readFile(__dirname + "../data/data.txt");
-const data = text.toString();
-const lines = data.split(/\r?\n/);
-function solve(scoring) {
-    let score = 0;
-    for (const line of lines) {
-        let hands = line.split(" ");
-        score += scoring[hands[0]][hands[1]];
-    }
-    return score;
-}
+const data = text.toString().split(/\r?\n/);
+const solve = (scoring) => data.map((l) => l.split(" ").reduce((a, b) => scoring[a][b])).reduce((a, b) => a + b);
 console.log(solve({ A: { X: 4, Y: 8, Z: 3 }, B: { X: 1, Y: 5, Z: 9 }, C: { X: 7, Y: 2, Z: 6 } }));
 console.log(solve({ A: { X: 3, Y: 4, Z: 8 }, B: { X: 1, Y: 5, Z: 9 }, C: { X: 2, Y: 6, Z: 7 } }));
 //# sourceMappingURL=app.js.map
--- 2/src/app.ts
+++ 2/src/app.ts
@@ -1,22 +1,11 @@
 import {promises as fs} from 'fs';
 import * as url from 'url';
-const __filename = url.fileURLToPath(import.meta.url);
+
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
-
 const text = await fs.readFile(__dirname + "../data/data.txt");
-const data : String = text.toString();
-const lines = data.split(/\r?\n/);
+const data : string[] = text.toString().split(/\r?\n/);
 
-function solve(scoring : Object) {
-    let score : number = 0;
-
-    for (const line of lines) {
-        let hands = line.split(" ");
-        score += scoring[hands[0]][hands[1]];
-    }
-
-    return score;
-}
+const solve = (scoring : Object) => data.map((l)=>l.split(" ").reduce((a,b)=>scoring[a][b])).reduce((a,b)=>a+b);
 
 console.log( solve({ A:{X:4,Y:8,Z:3}, B:{X:1,Y:5,Z:9}, C:{X:7,Y:2,Z:6}}) );
 console.log( solve({ A:{X:3,Y:4,Z:8}, B:{X:1,Y:5,Z:9}, C:{X:2,Y:6,Z:7}}) );
