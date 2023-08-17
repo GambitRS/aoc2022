e2150e55a4683b91cd8b8dfe6ccd48ecf485d604
GambitRS <hidde.baggelaar@gmail.com>
2023-08-18 01:45:27 +0200

    added some readability back

--- 3/dist/app.js
+++ 3/dist/app.js
@@ -5,15 +5,15 @@ const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 const text = await fs.readFile(__dirname + "../data/data.txt");
 const data = text.toString();
 //A = 65, Z = 90, a = 97, z = 122
-const solveArray = (a) => a.map((a) => a.map((a) => new Set(a))
-    .reduce((a, c, i) => new Set([...a].filter(i => c.has(i)))))
-    .map((a) => [...a][0])
-    .map((a) => (a.charCodeAt(0) > 90) ? a.charCodeAt(0) - 96 : a.charCodeAt(0) - 38)
-    .reduce((a, b) => a + b);
+const solveForElves = (elvesWithSacks) => elvesWithSacks.map((sacks) => sacks.map((items) => new Set(items))
+    .reduce((sack, otherSack) => new Set([...sack].filter(item => otherSack.has(item)))))
+    .map((sacks) => [...sacks][0])
+    .map((item) => (item.charCodeAt(0) > 90) ? item.charCodeAt(0) - 96 : item.charCodeAt(0) - 38)
+    .reduce((aPrio, bPrio) => aPrio + bPrio);
 //map reduce style
-console.log(solveArray(Array.from(data.matchAll(/.+/g))
+console.log(solveForElves(Array.from(data.matchAll(/.+/g))
     .map(String)
-    .map((a) => [a.slice(0, a.length / 2), a.slice(a.length / 2, a.length)])));
-console.log(solveArray(Array.from(data.matchAll(/.+\r?\n.+\r?\n.+/g))
-    .map((a) => String(a).split(/\r?\n/))));
+    .map((sack) => [sack.slice(0, sack.length / 2), sack.slice(sack.length / 2, sack.length)])));
+console.log(solveForElves(Array.from(data.matchAll(/.+\r?\n.+\r?\n.+/g))
+    .map((elves) => String(elves).split(/\r?\n/))));
 //# sourceMappingURL=app.js.map
--- 3/src/app.ts
+++ 3/src/app.ts
@@ -7,18 +7,18 @@ const text = await fs.readFile(__dirname + "../data/data.txt");
 const data : string = text.toString();
 
 //A = 65, Z = 90, a = 97, z = 122
-const solveArray = (a : string[][]) =>
-    a.map((a : string[]) => 
-        a.map((a)=>new Set(a))
-        .reduce((a,c,i)=>new Set([...a].filter(i=>c.has(i)))))
-    .map((a)=>[...a][0])
-    .map((a : string) => (a.charCodeAt(0) > 90) ? a.charCodeAt(0) - 96 : a.charCodeAt(0) - 38)
-    .reduce((a,b)=>a+b);
+const solveForElves = (elvesWithSacks : string[][]) =>
+    elvesWithSacks.map((sacks : string[]) => 
+        sacks.map((items)=>new Set(items))
+        .reduce((sack,otherSack)=>new Set([...sack].filter(item=>otherSack.has(item)))))
+    .map((sacks)=>[...sacks][0])
+    .map((item : string) => (item.charCodeAt(0) > 90) ? item.charCodeAt(0) - 96 : item.charCodeAt(0) - 38)
+    .reduce((aPrio,bPrio)=>aPrio+bPrio);
 
 //map reduce style
-console.log(solveArray(Array.from(data.matchAll(/.+/g))
+console.log(solveForElves(Array.from(data.matchAll(/.+/g))
     .map(String)        
-    .map((a)=>[a.slice(0,a.length/2), a.slice(a.length/2, a.length)])));
+    .map((sack)=>[sack.slice(0,sack.length/2), sack.slice(sack.length/2, sack.length)])));
 
-console.log(solveArray(Array.from(data.matchAll(/.+\r?\n.+\r?\n.+/g))        
-    .map((a)=>String(a).split(/\r?\n/))));
+console.log(solveForElves(Array.from(data.matchAll(/.+\r?\n.+\r?\n.+/g))        
+    .map((elves)=>String(elves).split(/\r?\n/))));
