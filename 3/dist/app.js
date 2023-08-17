e269b5aa5720951c89f0211014310516d4882a94
GambitRS <hidde.baggelaar@gmail.com>
2023-08-18 01:25:58 +0200

    map reduce style day 3

--- 3/dist/app.js
+++ 3/dist/app.js
@@ -4,52 +4,18 @@ const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 const text = await fs.readFile(__dirname + "../data/data.txt");
 const data = text.toString();
-const lines = data.split(/\r?\n/);
-function solve() {
-    let score = 0;
-    let score2 = 0;
-    let map0 = null, map1 = null, map2 = null;
-    for (let line of lines) {
-        let comp1 = line.slice(0, line.length / 2);
-        let comp2 = line.slice(line.length / 2, line.length);
-        var map = new Set();
-        for (let char of comp1) {
-            map.add(char);
-        }
-        //A = 65, Z = 90, a = 97, z = 122
-        for (let char of comp2) {
-            if (map.has(char)) {
-                score += (char.charCodeAt(0) > 90) ? char.charCodeAt(0) - 96 : char.charCodeAt(0) - 38;
-                break;
-            }
-        }
-        //part two        
-        for (let char of comp2) {
-            map.add(char);
-        }
-        let test;
-        //I want to use the map function of array for this exercise, so I made 2 sets into arrays instead of all maps
-        //This also allows me to use the some function, a map you can break out of using return
-        if (map0 == null)
-            map0 = Array.from(map);
-        else if (map1 == null)
-            map1 = map;
-        else {
-            map2 = map;
-            let doubles = [];
-            map0.map((entry) => { if (map1.has(entry))
-                doubles.push(entry); });
-            let test = doubles.some((char) => {
-                if (map2.has(char)) {
-                    return score2 += (char.charCodeAt(0) > 90) ? char.charCodeAt(0) - 96 : char.charCodeAt(0) - 38;
-                }
-            });
-            map0 = null;
-            map1 = map2 = null;
-        }
-    }
-    console.log(score);
-    console.log(score2);
-}
-solve();
+//A = 65, Z = 90, a = 97, z = 122
+const solveArray = (a) => a.map((a) => a.map((a) => new Set(a))
+    .reduce((a, c, i) => new Set([...a].filter(i => c.has(i)))))
+    .map((a) => [...a][0])
+    .map((a) => (a.charCodeAt(0) > 90) ? a.charCodeAt(0) - 96 : a.charCodeAt(0) - 38)
+    .reduce((a, b) => a + b);
+//map reduce style
+var sol1 = Array.from(data.matchAll(/.+/g))
+    .map(String)
+    .map((a) => [a.slice(0, a.length / 2), a.slice(a.length / 2, a.length)]);
+console.log(solveArray(sol1));
+var sol2 = Array.from(data.matchAll(/.+\r?\n.+\r?\n.+/g))
+    .map((a) => String(a).split(/\r?\n/));
+console.log(solveArray(sol2));
 //# sourceMappingURL=app.js.map
--- 3/src/app.ts
+++ 3/src/app.ts
@@ -5,58 +5,24 @@ const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 const text = await fs.readFile(__dirname + "../data/data.txt");
 const data : string = text.toString();
-const lines = data.split(/\r?\n/);
 
-function solve() {
-    let score : number = 0;
-    let score2 : number = 0;
-    let map0 : Array<string> = null, 
-        map1 : Set<string> = null, 
-        map2 : Set<string> = null;
+//A = 65, Z = 90, a = 97, z = 122
+const solveArray = (a : string[][]) =>
+    a.map((a : string[]) => 
+        a.map((a)=>new Set(a))
+        .reduce((a,c,i)=>new Set([...a].filter(i=>c.has(i)))))
+    .map((a)=>[...a][0])
+    .map((a : string) => (a.charCodeAt(0) > 90) ? a.charCodeAt(0) - 96 : a.charCodeAt(0) - 38)
+    .reduce((a,b)=>a+b);
 
-    for(let line of lines) {
-        let comp1 : string = line.slice(0,line.length/2);
-        let comp2 : string = line.slice(line.length/2,line.length);
-        var map : Set<string> = new Set<string>();
-    
-        for(let char of comp1) {
-            map.add(char);
-        }
+//map reduce style
+var sol1 = Array.from(data.matchAll(/.+/g))
+    .map(String)        
+    .map((a)=>[a.slice(0,a.length/2), a.slice(a.length/2, a.length)]);
 
-        //A = 65, Z = 90, a = 97, z = 122
-        for(let char of comp2) {
-            if(map.has(char)) {
-                score += (char.charCodeAt(0) > 90) ? char.charCodeAt(0) - 96 : char.charCodeAt(0) - 38;
-                break;
-            }
-        }
+console.log(solveArray(sol1));
 
-        //part two        
-        for(let char of comp2) {
-            map.add(char);
-        }
-        let test : number[];
+var sol2 = Array.from(data.matchAll(/.+\r?\n.+\r?\n.+/g))        
+    .map((a)=>String(a).split(/\r?\n/));
 
-        //I want to use the map function of array for this exercise, so I made 2 sets into arrays instead of all maps
-        //This also allows me to use the some function, a map you can break out of using return
-        if(map0 == null) map0 = Array.from(map);
-        else if(map1 == null) map1 = map;
-        else {
-            map2 = map;
-            let doubles : string[] = []
-            map0.map((entry)=>{if(map1.has(entry)) doubles.push(entry)});
-            let test = doubles.some((char)=>{if(map2.has(char)) {
-                return score2 += (char.charCodeAt(0) > 90) ? char.charCodeAt(0) - 96 : char.charCodeAt(0) - 38;
-            } });
-
-            map0 = null;
-            map1 = map2 = null;
-        }
-        
-    }   
-
-    console.log(score);
-    console.log(score2);
-}
-
-solve();
+console.log(solveArray(sol2));
